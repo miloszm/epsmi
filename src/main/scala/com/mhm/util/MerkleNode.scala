642ecd5c72937ca1504419416a75621fac8dacf7
Milosz Muszynski <miloszm@Miloszs-Mac-mini.local>
2020-10-26 14:18:08 +0100

    deserialize core format merkle proof - unit test

--- src/main/scala/com/mhm/util/MerkleNode.scala
+++ src/main/scala/com/mhm/util/MerkleNode.scala
@@ -8,7 +8,7 @@ sealed trait MerkleNode {
 case class StrMerkleNode(str: String) extends MerkleNode {
   override def isTuple = false
   override def value = str
-  override def toString: String = s"'$str'"
+  override def toString: String = s""""$str""""
 }
 
 case class TupleMerkleNode(left: MerkleNode, right: MerkleNode) extends MerkleNode {
