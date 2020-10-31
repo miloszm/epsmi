b55592fabc952dc03e9b27cd95022dc8a43f7859
Milosz Muszynski <miloszm@Miloszs-Mac-mini.local>
2020-10-31 11:30:17 +0100

    refactored int test fixture and added TrIdFromPos merkle true test

---
+++ src/test/scala/com/mhm/bitcoin/integration/test/IntTestFixture.scala
@@ -0,0 +1,29 @@
+package com.mhm.bitcoin.integration.test
+
+import com.googlecode.jsonrpc4j.StreamServer
+import com.mhm.api4electrum.Api4Electrum
+import com.mhm.rpcclient.{EpsmiClient, RpcClient}
+import com.mhm.rpcserver.RpcServer
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike}
+
+trait IntTestFixture extends FlatSpecLike with BeforeAndAfterAll {
+
+  val EXTERNAL_EPS_PORT = 50002
+  val EPSMI_PORT = 1420
+  val port = EPSMI_PORT
+
+  // -Djavax.net.ssl.keyStore=/Users/miloszm/proj/epsmi/rpcserver2.jks -Djavax.net.ssl.keyStorePassword=123456
+
+  case class Fixture( server: StreamServer, epsmiClient: EpsmiClient) {
+    def client(): Api4Electrum = epsmiClient.client
+  }
+
+  lazy val fixture = Fixture(RpcServer.startServer(port), RpcClient.createClient(port))
+
+  override protected def afterAll(): Unit = {
+    fixture.epsmiClient.close()
+    fixture.server.stop()
+    super.afterAll()
+  }
+
+}
