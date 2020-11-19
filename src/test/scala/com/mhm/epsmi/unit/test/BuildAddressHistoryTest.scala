5f111e0fb295dfe8efd9fbe8cffb07dead2c86ed
Milosz Muszynski <miloszm@Miloszs-Mac-mini.local>
2020-11-19 18:26:18 +0100

    all tests run again

--- src/test/scala/com/mhm/epsmi/unit/test/BuildAddressHistoryTest.scala
+++ src/test/scala/com/mhm/epsmi/unit/test/BuildAddressHistoryTest.scala
@@ -9,34 +9,34 @@ import org.scalatest.Matchers.{be, contain, convertToAnyShouldWrapper, empty}
 
 class BuildAddressHistoryTest extends FlatSpec {
 
-//  def assertAddressHistoryTx(addressHistory: AddressHistory, spk: String, height: Int, txId: String, subscribed: Boolean): Unit = {
-//    val historyElement = addressHistory.m.getOrElse(HashOps.script2ScriptHash(spk), fail)
-//    historyElement.history.head.height shouldBe height
-//    historyElement.history.head.txHash shouldBe txId
-//    if (height == 0)
-//      historyElement.history.head.fee shouldBe 0
-//    historyElement.subscribed shouldBe subscribed
-//  }
+  def assertAddressHistoryTx(addressHistory: AddressHistory, spk: String, height: Int, txId: String, subscribed: Boolean): Unit = {
+    val historyElement = addressHistory.m.getOrElse(HashOps.script2ScriptHash(spk), fail)
+    historyElement.history.head.height shouldBe height
+    historyElement.history.head.txHash shouldBe txId
+    if (height == 0)
+      historyElement.history.head.fee shouldBe 0
+    historyElement.subscribed shouldBe subscribed
+  }
 
-//  "transaction monitor" should "build single entry address history" in {
-//    val (dummySpk, blockHeight, dummyTx) = DummyTxCreator.createDummyFundingTx()
-//    val rpc = new DummyBtcRpc(Seq(dummyTx), Nil, Map(dummyTx.blockhash -> blockHeight))
-//    val monitor = new TransactionMonitor(rpc, false)
-//    val addressHistory = monitor.buildAddressHistory(Seq(dummySpk), Seq(new DummyDeterministicWallet))
-//    addressHistory.m.size shouldBe 1
-//    assertAddressHistoryTx(addressHistory, dummySpk, blockHeight, dummyTx.txId, subscribed = false)
-//  }
+  "transaction monitor" should "build single entry address history" in {
+    val (dummySpk, blockHeight, dummyTx) = DummyTxCreator.createDummyFundingTx()
+    val rpc = new DummyBtcRpc(Seq(dummyTx), Nil, Map(dummyTx.blockhash -> blockHeight))
+    val monitor = new TransactionMonitor(rpc, false)
+    val state = monitor.buildAddressHistory(Seq(dummySpk), Seq(new DummyDeterministicWallet))
+    state.addressHistory.m.size shouldBe 1
+    assertAddressHistoryTx(state.addressHistory, dummySpk, blockHeight, dummyTx.txId, subscribed = false)
+  }
 
-//  "transaction monitor" should " build address history with two entries" in {
-//    val(dummySpk1, containingBlockHeight1, dummyTx1) = DummyTxCreator.createDummyFundingTx()
-//    val(dummySpk2, containingBlockHeight2, dummyTx2) = DummyTxCreator.createDummyFundingTx()
-//    val rpc = new DummyBtcRpc(Seq(dummyTx1, dummyTx2), Nil, Map(dummyTx1.blockhash -> containingBlockHeight1, dummyTx2.blockhash -> containingBlockHeight2))
-//    val monitor = new TransactionMonitor(rpc, false)
-//    val addressHistory = monitor.buildAddressHistory(Seq(dummySpk1, dummySpk2), Seq(new DummyDeterministicWallet))
-//    addressHistory.m.size shouldBe 2
-//    assertAddressHistoryTx(addressHistory, dummySpk1, containingBlockHeight1, dummyTx1.txId, subscribed = false)
-//    assertAddressHistoryTx(addressHistory, dummySpk2, containingBlockHeight2, dummyTx2.txId, subscribed = false)
-//  }
+  "transaction monitor" should " build address history with two entries" in {
+    val(dummySpk1, containingBlockHeight1, dummyTx1) = DummyTxCreator.createDummyFundingTx()
+    val(dummySpk2, containingBlockHeight2, dummyTx2) = DummyTxCreator.createDummyFundingTx()
+    val rpc = new DummyBtcRpc(Seq(dummyTx1, dummyTx2), Nil, Map(dummyTx1.blockhash -> containingBlockHeight1, dummyTx2.blockhash -> containingBlockHeight2))
+    val monitor = new TransactionMonitor(rpc, false)
+    val state = monitor.buildAddressHistory(Seq(dummySpk1, dummySpk2), Seq(new DummyDeterministicWallet))
+    state.addressHistory.m.size shouldBe 2
+    assertAddressHistoryTx(state.addressHistory, dummySpk1, containingBlockHeight1, dummyTx1.txId, subscribed = false)
+    assertAddressHistoryTx(state.addressHistory, dummySpk2, containingBlockHeight2, dummyTx2.txId, subscribed = false)
+  }
 
   "transaction monitor" should " build address history with many entries" in {
     val(inputSpk, inputBlockHeight1, inputTx) = DummyTxCreator.createDummyFundingTx()
--- src/test/scala/com/mhm/epsmi/unit/test/SetupTest.scala
+++ src/test/scala/com/mhm/epsmi/unit/test/SetupTest.scala
@@ -12,8 +12,8 @@ class SetupTest extends FlatSpec {
   "setup" should "obtain list of script pub keys to monitor" in {
     val config = ConfigFactory.load()
     val result = new Setup(BitcoinSConnector.rpcCli, config).getScriptPubKeysToMonitor()
-    result.spksToMonitor.isEmpty shouldBe false
-    result.wallets.isEmpty shouldBe false
+    result.spksToMonitor.nonEmpty shouldBe true
+    result.wallets.nonEmpty shouldBe true
     println(s"getScriptPubKeysToMonitor result:")
     println(s"${result.spksToMonitor.size} spks to monitor, head is ${result.spksToMonitor.head}")
     println(s"wallets=${result.wallets}")
