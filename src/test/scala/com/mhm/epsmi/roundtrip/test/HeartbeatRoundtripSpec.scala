7290b4b46ba571d3994bf74f99eb9617e215b4df
Milosz Muszynski <miloszm@Miloszs-Mac-mini.local>
2020-11-29 00:23:00 +0100

    improved and enriched checking in HeartbeatRoundtripSpec

--- src/test/scala/com/mhm/epsmi/roundtrip/test/HeartbeatRoundtripSpec.scala
+++ src/test/scala/com/mhm/epsmi/roundtrip/test/HeartbeatRoundtripSpec.scala
@@ -3,7 +3,7 @@ package com.mhm.epsmi.roundtrip.test
 import java.io.ByteArrayOutputStream
 
 import com.mhm.api4electrum.{Api4ElectrumCore, Api4ElectrumImpl}
-import com.mhm.bitcoin.TransactionMonitorFactory
+import com.mhm.bitcoin.{HistoryElement, HistoryEntry, TransactionMonitorFactory}
 import com.mhm.epsmi.dummymonitor.DummyTxCreator.createDummyFundingTx
 import com.mhm.epsmi.dummymonitor.{DummyBtcRpc, DummyDeterministicWallet}
 import com.mhm.util.HashOps
@@ -13,20 +13,21 @@ import org.scalatest.Matchers.convertToAnyShouldWrapper
 class HeartbeatRoundtripSpec extends FlatSpec {
 
   val(dummySpk, containingBlockHeight, dummyTx) = createDummyFundingTx(confirmations = 0)
-
+  val sh = HashOps.script2ScriptHash(dummySpk)
   val rpc = DummyBtcRpc(Nil, Seq(dummyTx.vin), Map(dummyTx.blockhash -> containingBlockHeight))
   val monitor = TransactionMonitorFactory.create(rpc)
   val monitorState = monitor.buildAddressHistory(Seq(dummySpk), Seq(new DummyDeterministicWallet))
   monitorState.addressHistory.m.size shouldBe 1
-
-  val sh = HashOps.script2ScriptHash(dummySpk)
+  monitorState.addressHistory.m shouldBe Map(sh -> HistoryEntry(subscribed = false, List()))
   monitorState.getElectrumHistory(sh).getOrElse(fail).size shouldBe 0
 
   println(s"subscribing address: $sh")
   val monitorState2 = monitorState.subscribeAddress(sh)
+  monitorState2.addressHistory.m shouldBe Map(sh -> HistoryEntry(subscribed = true, List()))
 
   val (updatedTxs, monitorState3) = monitor.checkForUpdatedTxs(monitorState2)
   updatedTxs.isEmpty shouldBe true
+  monitorState3.updatedScripthashes shouldBe Nil
 
   println(s"unconfirmed transaction appears: ${dummyTx.txId}")
   val rpc2 = rpc.copy(txList = Seq(dummyTx))
@@ -39,10 +40,8 @@ class HeartbeatRoundtripSpec extends FlatSpec {
   protocol.triggerHeartbeatConnected(streamOutput)
 
   protocol.currentMonitorState.get().getElectrumHistory(sh).getOrElse(fail).size shouldBe 1
-
+  protocol.currentMonitorState.get().addressHistory.m shouldBe Map(sh -> HistoryEntry(subscribed = true,List(HistoryElement(dummyTx.txId,0,2))))
   val output = streamOutput.toString
-  output.contains(""""method": "blockchain.scripthash.subscribe"""") shouldBe true
-  output.contains(protocol.currentMonitorState.get.getElectrumHistoryHash(sh)) shouldBe true
-  output.contains(sh) shouldBe true
-  println(output)
+  val expectedHistoryHash = protocol.currentMonitorState.get.getElectrumHistoryHash(sh)
+  output shouldBe s"""{"method": "blockchain.scripthash.subscribe", "params": [$sh, $expectedHistoryHash]}"""
 }
