8810ab4be4e0973bd94216dd76c398f8c7385f0b
Milosz Muszynski <milosz.muszynski@gmail.com>
2020-12-09 16:16:07 +0100

    WIP address importer continued

--- src/main/scala/com/mhm/bitcoin/AddressImporter.scala
+++ src/main/scala/com/mhm/bitcoin/AddressImporter.scala
@@ -1,10 +1,12 @@
 package com.mhm.bitcoin
 
 import com.mhm.connectors.BitcoindRpcExtendedClient
+import com.mhm.connectors.RpcWrap.wrap
 import com.mhm.main.Constants
 import com.mhm.wallet.{DescriptorDeterministicWallet, DeterministicWallet}
 import grizzled.slf4j.Logging
 import org.bitcoins.commons.jsonmodels.bitcoind.RpcOpts
+import org.bitcoins.commons.jsonmodels.bitcoind.RpcOpts.ImportMultiRequest
 import org.bitcoins.core.protocol.BitcoinAddress
 import org.bitcoins.core.number.UInt32
 
@@ -19,25 +21,37 @@ object AddressImporter extends Logging {
   ): Unit = {
     logger.debug(s"Importing ${watchonlyAddresses.size} watch-only address(es) and ${wallets.size} wallets(s) into label ${Constants.ADDRESSES_LABEL}")
     val importMultiRequest = watchonlyAddresses.map { wa =>
-      RpcOpts.ImportMultiRequest(
+      ImportMultiRequest(
         scriptPubKey = RpcOpts.ImportMultiAddress(BitcoinAddress.fromString(wa)),
         timestamp = UInt32(System.currentTimeMillis()), // TODO refactor
-        label = Some(Constants.ADDRESSES_LABEL)
+        label = Some(Constants.ADDRESSES_LABEL),
+        watchonly = Some(true)
       )
     }.toVector
-    rpcCli.importMulti(importMultiRequest, rescan = false)
-    wallets.zipWithIndex.foreach { case (wallet, i) =>
-      logger.info(s"Importing wallet $i/${wallets.size}")
-      if (wallet.isInstanceOf[DescriptorDeterministicWallet]){
-        if (changeParam == 0 || changeParam == -1){
+    wrap(rpcCli.importMulti(importMultiRequest, rescan = false))
 
-        }
-        if (changeParam == 1 || changeParam == -1){
+    /**
+     * there is a problem here that eps uses "desc" and "range"
+     * which are missing in in ImportMultiRequest case class used by importMulti
+     * need to investigate it further
+     */
+    //    wallets.zipWithIndex.foreach { case (wallet, i) =>
+//      logger.info(s"Importing wallet $i/${wallets.size}")
+//      if (wallet.isInstanceOf[DescriptorDeterministicWallet]){
+//        if (changeParam == 0 || changeParam == -1){
+//          wrap(rpcCli.importMulti(Vector(ImportMultiRequest(
+//
+//            label = Some(Constants.ADDRESSES_LABEL),
+//            watchonly = Some(true)
+//          )), rescan = false))
+//        }
+//        if (changeParam == 1 || changeParam == -1){
+//
+//        }
+//      }
+//    }
 
-        }
-      }
-    }
-
+    logger.debug("Importing done")
     ()
   }
 
