dcd3286961102bd2d0e9aa69e9c196d36c57d7cc
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-01-02 10:13:57 +0100

    Improved 2 tests

--- src/test/scala/com/mhm/epsmi/monitor/unittest/UnrelatedTxTest.scala
+++ src/test/scala/com/mhm/epsmi/monitor/unittest/UnrelatedTxTest.scala
@@ -1,24 +1,22 @@
 package com.mhm.epsmi.monitor.unittest
 
-import com.mhm.bitcoin.{TransactionMonitor, TransactionMonitorFactory}
+import com.mhm.bitcoin.TransactionMonitorFactory
 import com.mhm.epsmi.dummymonitor.DummyTxCreator.createDummyFundingTx
 import com.mhm.epsmi.dummymonitor.{DummyBtcRpc, DummyDeterministicWallet}
-import com.mhm.util.HashOps
-import com.mhm.util.HashOps.script2ScriptHash
 import org.scalatest.FlatSpec
-
-//  ###transaction that has nothing to do with our wallet
 
 class UnrelatedTxTest extends FlatSpec with AddressHistoryAssertions {
 
-  val(dummySpk, containingBlockHeight, dummyTx) = createDummyFundingTx(confirmations = 0)
-  val ourDummySpk = "beef"*16
+  "transaction that has nothing to do with our wallet" should "not enter history" in {
+    val (_, containingBlockHeight, dummyTx) = createDummyFundingTx(confirmations = 0)
+    val ourDummySpk = "beef" * 16
 
-  val rpc = DummyBtcRpc(Seq(dummyTx), Seq(dummyTx.vin), Map(dummyTx.blockhash -> containingBlockHeight))
-  val monitor = TransactionMonitorFactory.create(rpc)
+    val rpc = DummyBtcRpc(Seq(dummyTx), Seq(dummyTx.vin), Map(dummyTx.blockhash -> containingBlockHeight))
+    val monitor = TransactionMonitorFactory.create(rpc)
 
-  val monitorState = monitor.buildAddressHistory(Seq(ourDummySpk), Seq(new DummyDeterministicWallet))
-  monitorState.addressHistory.m.size shouldBe 1
-  monitorState.getElectrumHistory(script2ScriptHash(ourDummySpk)) shouldBe Some(Nil)
+    val monitorState = monitor.buildAddressHistory(Seq(ourDummySpk), Seq(new DummyDeterministicWallet))
+    monitorState.addressHistory.m.size shouldBe 1
+    assertHistoryEmpty(monitorState.addressHistory, ourDummySpk)
+  }
 
 }
