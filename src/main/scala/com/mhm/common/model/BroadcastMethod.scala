f9353f0c5a57f76f335ffd4578bc5c05211ce4b1
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-01-05 11:32:45 +0100

    documentation - transaction.broadcast

--- src/main/scala/com/mhm/common/model/BroadcastMethod.scala
+++ src/main/scala/com/mhm/common/model/BroadcastMethod.scala
@@ -2,5 +2,5 @@ package com.mhm.common.model
 
 sealed trait BroadcastMethod
 
-object OwnNode extends  BroadcastMethod
+object OwnNode extends BroadcastMethod
 object UnsupportedBroadcastMethod extends BroadcastMethod
