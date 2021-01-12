caf44fe8608df22d1e7967d76edb5da9f63a1c08
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-01-12 18:26:28 +0100

    more tests for transaction monitor state

--- src/test/scala/com/mhm/epsmi/monitor/unittest/TransactionMonitorStateTest.scala
+++ src/test/scala/com/mhm/epsmi/monitor/unittest/TransactionMonitorStateTest.scala
@@ -37,4 +37,49 @@ class TransactionMonitorStateTest extends FlatSpec {
     getHeights("sh3") should contain theSameElementsInOrderAs Seq(900, 998, 999, 1000)
   }
 
+  "add unconfirmed shs" should "add list of shs to an existing tx unconfirmed entry if it exists" in {
+    val unconfirmed = Map(
+      "tx1" -> Seq("sh1", "sh2")
+    )
+    val state = TransactionMonitorState(AddressHistory(Map()), unconfirmedTxes = unconfirmed).addUnconfirmedScripthases("tx1", Seq("sh3", "sh4"))
+    state.unconfirmedTxes.getOrElse("tx1", fail) should contain theSameElementsAs Seq("sh1", "sh2", "sh3", "sh4")
+  }
+
+  "add unconfirmed shs" should "also work if tx was not in the list" in {
+    val unconfirmed = Map(
+      "tx1" -> Seq("sh1", "sh2")
+    )
+    val state = TransactionMonitorState(AddressHistory(Map()), unconfirmedTxes = unconfirmed).addUnconfirmedScripthases("tx2", Seq("sh3", "sh4"))
+    state.unconfirmedTxes.getOrElse("tx2", fail) should contain theSameElementsAs Seq("sh3", "sh4")
+  }
+
+  "add unconfirmed shs" should "also work if the pre-existing shs list was empty" in {
+    val unconfirmed = Map(
+      "tx1" -> Seq()
+    )
+    val state = TransactionMonitorState(AddressHistory(Map()), unconfirmedTxes = unconfirmed).addUnconfirmedScripthases("tx1", Seq("sh1", "sh2"))
+    state.unconfirmedTxes.getOrElse("tx1", fail) should contain theSameElementsAs Seq("sh1", "sh2")
+  }
+
+  "add unconfirmed shs" should "eliminate duplicated shs" in {
+    val unconfirmed = Map(
+      "tx1" -> Seq("sh1")
+    )
+    val state = TransactionMonitorState(AddressHistory(Map()), unconfirmedTxes = unconfirmed).addUnconfirmedScripthases("tx1", Seq("sh1", "sh1"))
+    state.unconfirmedTxes.getOrElse("tx1", fail) should contain theSameElementsAs Seq("sh1")
+  }
+
+  "update height for shs" should "update height in address history for all given shs" in {
+    val addressHistory = AddressHistory(Map(
+      "sh1" -> HistoryEntry(subscribed = true, Seq(HistoryElement("tx2", -1), HistoryElement("tx2", 0), HistoryElement("tx3", 500000), HistoryElement("tx3", 600000))),
+      "sh2" -> HistoryEntry(subscribed = true, Seq(HistoryElement("tx3", 0), HistoryElement("tx3", 20), HistoryElement("tx3", 50), HistoryElement("tx3", 10))),
+      "sh3" -> HistoryEntry(subscribed = true, Seq(HistoryElement("tx3", 1000), HistoryElement("tx2", 999), HistoryElement("tx3", 998), HistoryElement("tx2", 900))),
+    ))
+    val state = TransactionMonitorState(addressHistory).updateHeightForScripthashes(Seq("sh1", "sh2", "sh3"), "tx3", 20)
+    def getHeights(sh: String) = state.addressHistory.m.getOrElse(sh, fail).history.map(_.height)
+    getHeights("sh1") should contain theSameElementsInOrderAs Seq(-1, 0, 20, 20)
+    getHeights("sh2") should contain theSameElementsInOrderAs Seq.fill(4)(20)
+    getHeights("sh3") should contain theSameElementsInOrderAs Seq(20, 999, 20, 900)
+  }
+
 }
