580328dda308b93bea1ed60f74bb4bbb002efad6
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-01-19 10:36:58 +0100

    cleaned up RpcClient

--- src/main/scala/com/mhm/rpcclient/RpcClient.scala
+++ src/main/scala/com/mhm/rpcclient/RpcClient.scala
@@ -26,7 +26,6 @@ case class EpsmiClient(client: Api4Electrum, socket: Socket){
 
 object RpcClient extends App {
     val port = 50002
-//    val port = 1420
   var socket: Socket = null //TODO
 
   def createClient(port: Int = port): EpsmiClient = {
@@ -57,71 +56,64 @@ object RpcClient extends App {
     EpsmiClient(client, socket)
   }
 
-  def doServerVersion: Unit = {
+  def performServerApiCalls: Unit = {
     val epsmiClient = createClient()
     val client = epsmiClient.client
 
-    println
+    println("=========== server.version =============")
     val result = client.serverVersion("1.4", "1.4")
     println(s"result of server.version:")
     println(s"size = ${result.length}")
     result.zipWithIndex.foreach{ case (e, i) => println(s"$i = $e")}
 
-//    println
-//    val hexTry = Try(client.blockchainBlockHeader(652221))
-//    hexTry.fold(
-//      { e =>
-//        println(s"json RPC exception caught: $e")
-//        if (e.isInstanceOf[JsonRpcClientException]) println(s"code = ${e.asInstanceOf[JsonRpcClientException].getCode}")
-//      },
-//      { hex =>
-//        println(s"result of blockchain.block.header:")
-//        println(s"hex = $hex")
-//      }
-//    )
+    println("=========== blockchain.block.header =============")
+    val hexTry = Try(client.blockchainBlockHeader(652221))
+    hexTry.fold(
+      { e =>
+        println(s"json RPC exception caught: $e")
+        if (e.isInstanceOf[JsonRpcClientException]) println(s"code = ${e.asInstanceOf[JsonRpcClientException].getCode}")
+      },
+      {
+        hex => println(s"result hex = $hex")
+      }
+    )
 
-//    println
-//    val header = client.blockchainBlockGetHeader(652221)
-//    println(header)
+    println("=========== blockchain.block.get_header =============")
+    val header = client.blockchainBlockGetHeader(652221)
+    println(header)
 
-//    println
-//    val fee = client.estimateFee(1)
-//    println(s"fee=$fee")
+    println("=========== blockchain.estimatefee =============")
+    val fee = client.estimateFee(1)
+    println(s"fee=$fee")
 
-//    println
-//    val chunk = client.blockchainBlockGetChunk(323)
-//    println(s"chunk=$chunk")
+    println("=========== blockchain.block.get_chunk =============")
+    val chunk = client.blockchainBlockGetChunk(323)
+    println(s"chunk=$chunk")
 
-//    println
-//    val headersResult = client.blockchainBlockHeaders(652510, 512)
-//    println(s"headers result = $headersResult")
+    println("=========== blockchain.block.headers =============")
+    val headersResult = client.blockchainBlockHeaders(652510, 512)
+    println(s"headers result = $headersResult")
 
-    println
-//    val transactionHex = client.blockchainTransactionGet("b850bd9f727888019ddd5481124b83c17b9dd263fe4c7c007a0a6c0f4c0f1573")
-//    val transactionHex = client.blockchainTransactionGet("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b")
-//    val transactionHex = client.blockchainTransactionGet("0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098")
-//    println(s"transaction result = $transactionHex")
+    println("=========== blockchain.transaction.get =============")
+    val transactionHex = client.blockchainTransactionGet("0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098")
+    println(s"transaction result = $transactionHex")
 
-//    println
-//    val transactionId = client.blockchainTrIdFromPos(652742, 5, false)
-//    println(s"trIdFromPos = $transactionId")
-//    val trHex = client.blockchainTransactionGet(transactionId)
-//    println(s"trHex for the above = $trHex")
+    println("=========== blockchain.transaction.id_from_pos =============")
+    val transactionId = client.blockchainTrIdFromPos(652742, 5, false)
+    println(s"trIdFromPos = $transactionId")
+    val trHex = client.blockchainTransactionGet(transactionId)
+    println(s"trHex for the above = $trHex")
 
-//    println
-//    val merkleResult: MerkleResult =
-//      if (port == 50002) {
-//        client.blockchainTrIdFromPosMerkleTrue(652742, 5, true)
-//      } else {
-//        val s = client.blockchainTrIdFromPos(652742, 5, true)
-//        val objectMapper = new ObjectMapper()
-//        objectMapper.readValue[MerkleResult](s, classOf[MerkleResult])
-//      }
-//    println(s"txHash= ${merkleResult.txHash}")
-//    println("merkle=")
-//    merkleResult.merkle.foreach{println(_)}
+    println("=========== blockchain.transaction.id_from_pos_merkle_true =============")
+    val merkleResult: MerkleResult = {
+      val s = client.blockchainTrIdFromPos(652742, 5, true)
+      new ObjectMapper().readValue[MerkleResult](s, classOf[MerkleResult])
+    }
+    println(s"txHash= ${merkleResult.tx_hash}")
+    println("merkle=")
+    merkleResult.merkle.foreach{println(_)}
 
-    println
+    println("=========== blockchain.transaction.get_merkle =============")
     val txId4GetMerkle = client.blockchainTrIdFromPos(652742, 5, false) // otherwise it won't be found
     val merkle = client.blockchainTransactionGetMerkle(txId4GetMerkle, 0)
     println(s"get merkle result = ")
@@ -130,48 +122,30 @@ object RpcClient extends App {
     println(s"   merkle=")
     merkle.merkle.foreach{println(_)}
 
-//    val socketInputStream = socket.getInputStream
-//    while (true) {
-//      println(socketInputStream.read().toChar)
-//    }
+    println("=========== blockchain.scripthash.subscribe =============")
+    // you need to have actual wallet for that created
+    val subscriptionResponse = client.blockchainScripthashSubcribe("5be022609383d23e2d545b3b359446466c269686c1e697b60355424ed30490d2")
+    println(s"blockchainScripthashSubcribe result = ")
+    println(s"   $subscriptionResponse")
 
-//    println
-    // you need to have actual wallet for that created, so this is really an
-    // integration test
-//    val subscriptionResponse = client.blockchainScripthashSubcribe("5be022609383d23e2d545b3b359446466c269686c1e697b60355424ed30490d2")
-//    println(s"blockchainScripthashSubcribe result = ")
-//    println(s"   $subscriptionResponse")
+    println("=========== blockchain.headers.subscribe =============")
+    val headersSubscribeResult = client.blockchainHeadersSubcribe()
+    println(s"blockchainHeadersSubcribe result = ")
+    println(s"   hash=${headersSubscribeResult.hex}")
+    println(s"   height=${headersSubscribeResult.height}")
 
-//    println
-//    val headersSubscribeResult = client.blockchainHeadersSubcribe()
-//    println(s"blockchainHeadersSubcribe result = ")
-//    println(s"   hash=${headersSubscribeResult.hex}")
-//    println(s"   height=${headersSubscribeResult.height}")
+    println("=========== blockchain.scripthash.get_history =============")
+    val history = client.blockchainScripthashGetHistory("5be022609383d23e2d545b3b359446466c269686c1e697b60355424ed30490d2")
+    println(s"blockchainScripthashGetHistory result = ${history.map(_.toString).mkString("|")}")
 
-//    println
-//    val history = client.blockchainScripthashGetHistory("5be022609383d23e2d545b3b359446466c269686c1e697b60355424ed30490d2")
-//    println(s"blockchainScripthashGetHistory result = ${history.map(_.toString).mkString("|")}")
+    println("=========== server.ping =============")
+    val pingResponse = client.serverPing()
+    println(s"ping response = $pingResponse")
 
-//    println
-//    val pingResponse = client.serverPing()
-//    println(s"ping response = $pingResponse")
-
-    println
+    println("=========== blockchain.scripthash.get_balance =============")
     val balanceResponse = client.blockchainScripthashGetBalance("5be022609383d23e2d545b3b359446466c269686c1e697b60355424ed30490d2")
     println(s"balanceResponse = $balanceResponse")
 
-//    Thread.sleep(60000L)
-//
-//    println("2nd time")
-//    val txId4GetMerkle2 = client.blockchainTrIdFromPos(652742, 5, false) // otherwise it won't be found
-//    val merkle2 = client.blockchainTransactionGetMerkle(txId4GetMerkle2)
-//    println(s"get merkle result = ")
-//    println(s"   blockHeight=${merkle2.blockHeight}")
-//    println(s"   pos=${merkle2.pos}")
-//    println(s"   merkle=")
-//    merkle2.merkle.foreach{println(_)}
-//
-//    Thread.sleep(3000000L)
     epsmiClient.close()
   }
 
@@ -180,6 +154,5 @@ object RpcClient extends App {
     socketFactory.createSocket(address, port)
   }
 
-
-  doServerVersion
+  performServerApiCalls
 }
